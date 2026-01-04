ef904ddaa4be49f7f71df498ea6cebe7c3fe3975
Jyoti Sahoo <jyotishankarsahoo86@gmail.com>
2026-01-04 07:39:29 -0600

    Tool definition for search, email and weather

--- Foundation/08_agentWithOtherMiddleware.ts
+++ Foundation/08_agentWithOtherMiddleware.ts
@@ -1,9 +1,46 @@
-import { createAgent } from "langchain";
+import { createAgent, tool } from "langchain";
+import { z } from "zod";
 
-
+const searchTool = tool(
+    (query) => {
+        return `Search result for ${query}: Found 5 Articles`;
+    },
+    {
+        name: "search_tool",
+        description: "Search internet for information",
+        schema: z.object({
+            query: z.string(),
+        }),
+    }
+);
+const weatherTool = tool(
+    (input) => {
+        return `Weather is sunny in ${input.city}`;
+    },
+    {
+        name: "get_weather_info",
+        description: "Retrieve weather info from Location data",
+        schema: z.object({
+            city: z.string(),
+        }),
+    }
+);
+const emailTool = tool(
+    (recipient, subject) => {
+        return `Email sent to ${recipient} with subject: ${subject}`;
+    },
+    {
+        name: "email_tool",
+        description: "Send email to some one",
+        schema: z.object({
+            recipient: z.string(),
+            subject: z.string(),
+        }),
+    }
+);
 const agent = createAgent({
     model: "",
-    tools: [],
+    tools: [searchTool, weatherTool, emailTool],
     middleware: [],
 });
 
