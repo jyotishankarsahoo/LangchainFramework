5acd5e3d5931f7590ac8ee57cf30533f40d31634
Jyoti Sahoo <jyotishankarsahoo86@gmail.com>
2026-01-05 09:47:28 -0600

    Split documents into chunk

--- RAG/ragAgent.ts
+++ RAG/ragAgent.ts
@@ -1,4 +1,5 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 // Load Docs using PDFLoader
 const loader = new PDFLoader("./RAG/docs/nke-10k-2023.pdf");
@@ -6,3 +7,9 @@ const docs = await loader.load();
 console.log(docs.length);
 
 // Split all Docs in to Chunks
+const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+});
+const chunks = await splitter.splitDocuments(docs);
+console.log(chunks.length);
