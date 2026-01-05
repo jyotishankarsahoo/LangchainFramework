f4ce9e4732738b859fdaac5b66ada298a5273598
Jyoti Sahoo <jyotishankarsahoo86@gmail.com>
2026-01-05 09:58:30 -0600

    Add Embedding and store all in to Vector Store

--- RAG/ragAgent.ts
+++ RAG/ragAgent.ts
@@ -1,5 +1,7 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import {MemoryVectorStore} from "@langchain/classic/vectorstores/memory"
 
 // Load Docs using PDFLoader
 const loader = new PDFLoader("./RAG/docs/nke-10k-2023.pdf");
@@ -13,3 +15,14 @@ const splitter = new RecursiveCharacterTextSplitter({
 });
 const chunks = await splitter.splitDocuments(docs);
 console.log(chunks.length);
+
+// Define Embedding to be used on Chunks
+const embedding = new OpenAIEmbeddings({
+    model: "text-embedding-3-large",
+});
+
+// Define a Vector Store to Store Embedding
+const vectorStore = new MemoryVectorStore(embedding)
+
+// Store Chunks in to Vector Store using Embedding
+await vectorStore.addDocuments(chunks)
