e7e0ec130ab553c78f9151e62349abbf4b375bde
Jyoti Sahoo <jyotishankarsahoo86@gmail.com>
2026-01-07 10:17:02 -0600

    Add Persistent storage for conversation history

--- RAG/05_ragAgentServer.ts
+++ RAG/05_ragAgentServer.ts
@@ -1,31 +1,16 @@
 import { createAgent, tool } from "langchain";
+import { Document } from "@langchain/core/documents";
+import { MemorySaver } from "@langchain/langgraph";
 import { z } from "zod";
 import "dotenv/config";
-import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
-import { OpenAIEmbeddings } from "@langchain/openai";
-import { MemoryVectorStore } from "@langchain/classic/vectorstores/memory";
-
-const loader = new PDFLoader("./RAG/docs/nke-10k-2023.pdf");
-const docs = await loader.load();
-const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 1000,
-    chunkOverlap: 200,
-});
-const chunks = await splitter.splitDocuments(docs);
-const embedding = new OpenAIEmbeddings({ model: "text-embedding-3-large" });
-const vectorDB = new MemoryVectorStore(embedding);
-await vectorDB.addDocuments(chunks);
-const retriever = vectorDB.asRetriever({
-    searchType: "mmr",
-    searchKwargs: { fetchK: 1 },
-});
+import { ragRetriever } from "./retriever";
 
 const retrieverTool = tool(
     async ({ query }) => {
-        const retrievedDocs = await retriever.invoke(query);
+        const retriever = await ragRetriever();
+        const retrievedDocs: Document[] = await retriever.invoke(query);
         const docContent = retrievedDocs
-            .map((doc) => {
+            .map((doc: Document) => {
                 return doc.pageContent;
             })
             .join("\n\n");
@@ -64,9 +49,12 @@ const emailTool = tool(
         }),
     }
 );
+
+const memorySaver = new MemorySaver();
 const agent = createAgent({
     model: "claude-sonnet-4-5-20250929",
     tools: [weatherTool, retrieverTool, emailTool],
+    checkpointer: memorySaver,
     systemPrompt:
         "You are a helpful question and answer assistant. You have access to tools that retrieves context from PDF documents, get weather tool and send email tool.Use the tools to help answer user query ",
 });
